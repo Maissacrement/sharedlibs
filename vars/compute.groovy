401ea8debb5fe40a84d5b961ec0f696199e70e74
delar <emmariodelar@gmail.com>
2022-04-16 08:42:54 +0200

    Correct docker import

--- vars/compute.groovy
+++ vars/compute.groovy
@@ -1,6 +1,5 @@
-import org.jenkinsci.plugins.docker.workflow.*
-
 def addition(x, y) {
     echo "${x} and ${y} do ${x+y}"
+    def docker = org.jenkinsci.plugins.docker.workflow.Docker docker
     docker.image('mysql:5')
 }
