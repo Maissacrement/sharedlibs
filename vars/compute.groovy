75020919f5a826cb0a53d9e824c94749018e732f
delar <emmariodelar@gmail.com>
2022-04-16 09:13:12 +0200

    Correct docker import

--- vars/compute.groovy
+++ vars/compute.groovy
@@ -2,6 +2,6 @@ import org.jenkinsci.plugins.docker.workflow.*
 
 def addition(x, y) {
     echo "${x} and ${y} do ${x+y}"
-    Object docker = new DockerDSL().getValue(script)
+    Object docker = new workflow.DockerDSL().getValue(script)
     println docker
 }
